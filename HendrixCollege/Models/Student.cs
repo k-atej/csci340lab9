ef4a3fe60e786a73371792fe8e1d6dd65caf801c
k-atej <k.atej@comcast.net>
2023-10-30 12:28:00 -0500

    Added Age to tables

--- HendrixCollege/Models/Student.cs
+++ HendrixCollege/Models/Student.cs
@@ -5,6 +5,7 @@ namespace HendrixCollege.Models
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+        public int Age { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
